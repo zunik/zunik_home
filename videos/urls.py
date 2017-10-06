2cb485d745a5d8a1f008dabfb3d9f157a7567a0a
chazunik <chazunik@gmail.com>
2017-10-06 17:57:50 +0900

    video year url change

--- videos/urls.py
+++ videos/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'video'
 urlpatterns =[
     url(r'^$', views.my_video_redirect, name='list'),
-    url(r'^(?P<year>\d{4})/$', views.MyVideoYearView.as_view(), name='list_year'),
+    url(r'^year/(?P<year>\d{4})/$', views.MyVideoYearView.as_view(), name='list_year'),
     url(r'^(?P<pk>[0-9]+)/$', views.MyVideoDetailView.as_view(), name='detail_view'),
     url(r'^tag/(?P<tag>[^/]+(?u))/$', views.MyVideoTagView.as_view(), name='list_tag'),
 ]
