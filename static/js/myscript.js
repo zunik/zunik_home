0e20cc2f17343d5491c258d4b01edfe9d42f731f
chazunik <chazunik@gmail.com>
2017-10-13 23:15:10 +0900

    youtube query string

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -6,12 +6,23 @@ $(function(){
         "<div class='embed-responsive embed-responsive-16by9 col-md-6'>" +
         "<iframe class='embed-responsive-item' src='//www.youtube.com/embed/";
 
-        var youtube_end = "?rel=0' allowfullscreen></iframe></div></div>";
+        var youtube_end = "' allowfullscreen></iframe></div></div>";
 
         $youtube_markdown.each(function () {
             var youtube_id = $(this).attr("data-id")
+
+            var query_string = '?rel=0'
+
+            if ($(this).is('[data-start]')) {
+                query_string += '&start=' + $(this).attr("data-start")
+            }
+
+            if ($(this).is('[data-end]')) {
+                query_string += '&end=' + $(this).attr("data-end")
+            }
+
             $(this).removeClass("youtube").addClass("container mb-3");
-            $(this).html(youtube_start + youtube_id + youtube_end)
+            $(this).html(youtube_start + youtube_id + query_string + youtube_end)
         })
     }
 })
