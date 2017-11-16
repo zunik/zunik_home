8dfd1d75b64e7d47775dca8dfaba2982648d3f5e
chazunik <chazunik@gmail.com>
2017-11-17 00:18:01 +0900

    year_list delete

--- diary/urls.py
+++ diary/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 app_name = 'diary'
 urlpatterns =[
-    url(r'^open/$', views.open_diary_redirect, name='open_list'),
-    url(r'^open/year/(?P<year>\d{4})/$', views.OpenDiaryYearView.as_view(), name='open_list_year'),
+    url(r'^open/$', views.OpenDiaryListView.as_view(), name='open_list'),
     url(r'^open/tag/(?P<tag>[^/]+(?u))/$', views.OpenDiaryTagView.as_view(), name='open_list_tag'),
     url(r'^open/(?P<pk>[0-9]+)/$', views.OpenDiaryDetailView.as_view(), name='open_detail'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,31 +1,17 @@
-from django.shortcuts import HttpResponseRedirect, reverse
-from django.views.generic.dates import YearArchiveView
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from tagging.views import TaggedObjectList
 from .models import Diary
 
 
-class OpenDiaryYearView(YearArchiveView):
-    queryset = Diary.objects.filter(hide=False)
-    date_field = 'diary_at'
-    make_object_list = True
+class OpenDiaryListView(ListView):
+    model = Diary
     paginate_by = 10
     template_name = 'diary/diary_list.html'
 
     def get_context_data(self, **kwargs):
-        context = super(OpenDiaryYearView, self).get_context_data(**kwargs)
-        context['now_year'] = int(self.get_year())
-        context['year_list'] = list(Diary.objects.filter(hide=False).dates('diary_at', 'year', order='DESC'))
+        context = super(OpenDiaryListView, self).get_context_data(**kwargs)
 
-        i = 0
-        for date in context['year_list']:
-            year = context['year_list'][i].year
-            context['year_list'][i] = {}
-            context['year_list'][i]['year'] = year
-            context['year_list'][i]['count'] = Diary.objects.filter(diary_at__year=date.year, hide=False).count()
-            i = i + 1
-
-        context['list_type'] = 'year'
+        context['list_type'] = 'all'
         return context
 
 
@@ -36,15 +22,6 @@ class OpenDiaryTagView(TaggedObjectList):
 
     def get_context_data(self, **kwargs):
         context = super(OpenDiaryTagView, self).get_context_data(**kwargs)
-        context['year_list'] = list(Diary.objects.filter(hide=False).dates('diary_at', 'year', order='DESC'))
-
-        i = 0
-        for date in context['year_list']:
-            year = context['year_list'][i].year
-            context['year_list'][i] = {}
-            context['year_list'][i]['year'] = year
-            context['year_list'][i]['count'] = Diary.objects.filter(diary_at__year=date.year, hide=False).count()
-            i = i + 1
 
         context['now_tag'] = context['tag']
         context['list_type'] = 'tag'
@@ -54,10 +31,3 @@ class OpenDiaryTagView(TaggedObjectList):
 
 class OpenDiaryDetailView(DetailView):
     queryset = Diary.objects.filter(hide=False)
-
-
-def open_diary_redirect(request):
-    last_diary = Diary.objects.filter(hide=False).latest('diary_at')
-    year = last_diary.diary_at.year
-
-    return HttpResponseRedirect(reverse('diary:open_list_year', args=(year,)))
