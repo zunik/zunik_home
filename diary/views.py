4310a5e1f0e3876f6d45408ae19f4eb516684998
chazunik <chazunik@gmail.com>
2017-11-23 01:43:10 +0900

    back list page and tag

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import DetailView, ListView
 from tagging.views import TaggedObjectList
-from tagging.models import TaggedItem
+from tagging.models import TaggedItem, Tag
 from .models import Diary
 from zunik_home.help import custom_paginator
 
@@ -39,16 +39,32 @@ class OpenDiaryDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(OpenDiaryDetailView, self).get_context_data(**kwargs)
+
         object_path = '/diary/open/' + str(context['object'].id) + '/';
         context['mentioned_list'] = Diary.objects.filter(content__icontains=object_path)
 
         relation_num = 5
         context['related_list'] = TaggedItem.objects.get_related(context['object'], Diary, relation_num)
 
-        context['next_object'] = Diary.objects.filter(diary_at__gte=context['object'].diary_at)\
+        # 목록으로 돌아갈시 page
+        now_page = self.request.GET.get('page')
+        if not now_page:
+            now_page = 1
+        context['now_page'] = now_page
+
+        now_tag = self.request.GET.get('tag')
+
+        if now_tag:
+            tag_object = Tag.objects.get(name=now_tag)
+            objects = TaggedItem.objects.get_by_model(Diary, tag_object)
+            context['now_tag'] = now_tag
+        else:
+            objects = Diary.objects
+
+        context['next_object'] = objects.filter(diary_at__gte=context['object'].diary_at)\
             .exclude(diary_at=context['object'].diary_at, id__lte=context['object'].id).order_by('-diary_at','-id').last()
 
-        context['prev_object'] = Diary.objects.filter(diary_at__lte=context['object'].diary_at)\
+        context['prev_object'] = objects.filter(diary_at__lte=context['object'].diary_at)\
             .exclude(diary_at=context['object'].diary_at, id__gte=context['object'].id).order_by('-diary_at', '-id').first()
 
         return context
