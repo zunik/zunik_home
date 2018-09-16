193ad194c36a36136ed3f5f12c0ac49f18389b3e
zunik <zunik@buzzni.com>
2018-09-16 23:57:52 +0900

    진행중

--- app/zunik_home/settings.py
+++ app/zunik_home/settings.py
@@ -139,7 +139,7 @@ TIME_ZONE = 'Asia/Seoul'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 
 STATIC_URL = '/static/'
 
